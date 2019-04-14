cb2f9ccaeca311c106d243f6fcd6505c934a2a36
JamesHoster <48329994+JamesHoster@users.noreply.github.com>
2019-04-14 15:28:03 -0400

    Add files via upload

---
+++ Palindrome/Palindrome/Palindrome.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Palindrome
+    {
+        public bool IsPalindrome(string userInput)
+        {
+            
+                char[] chArray = userInput.ToCharArray();
+
+                string forward = userInput.Substring(0, userInput.Length / 2);
+                // char[] chArray = msg.ToCharArray();
+
+                Array.Reverse(chArray);
+
+                string temp = new string(chArray);
+                string backward = temp.Substring(0, temp.Length / 2);
+
+                /*
+                     char[] forward = chArray;
+
+                      Array.Reverse(chArray);
+
+                     char[] backward = chArray;
+                     */
+                if (forward == backward)
+                {
+                    Console.WriteLine($"{userInput} is a Palindrome");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"{userInput} is NOT a Palindrome");
+                    return false;
+                }
+
+         
+            
+        }
+    }
+}
---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string msg = "";
+            try
+            {
+                do
+                {
+                    Console.Write("Is it a Palindrome? Enter your text to find out now! ");
+                    msg = Console.ReadLine();
+                    if(msg.Length >= 50)
+                    {
+                        throw new OverflowException();
+                    }
+                    //char[] chArray = msg.ToCharArray();
+
+                    Palindrome myPal = new Palindrome();
+                    myPal.IsPalindrome(msg);
+
+                    Console.WriteLine("");
+                    Console.ReadLine();
+                } while (msg.ToUpper() != "EXIT");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("There is a maximum character length of 50.");
+            }
+        }
+    }
+ }   
+
